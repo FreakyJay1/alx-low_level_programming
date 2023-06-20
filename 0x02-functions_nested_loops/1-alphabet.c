a65e15385f261c5d86c663d999ea7f6179807741
FreakyJay1 <jjthebestmanmode@gmail.com>
2023-06-20 04:00:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - is to print all the alphabet in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for(c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
