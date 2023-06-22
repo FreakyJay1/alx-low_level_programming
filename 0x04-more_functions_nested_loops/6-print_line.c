aa8d39619700415983ef5b26e11e35a4ca32e106
FreakyJay1 <jjthebestmanmode@gmail.com>
2023-06-22 07:25:27 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,17 +5,20 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i;
+	int i;
 
-		for (i = 10; i <= n; i++)
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
 		{
-			_putchar('_');
+			_putchar(95);
 		}
+
+		_putchar('\n');
+	}
+
+	else
+	{
 		_putchar('\n');
 	}
 }
