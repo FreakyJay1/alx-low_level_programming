8f9ac7b2ef7cf66d917ef283dff27ef5a97fd745
FreakyJay1 <jjthebestmanmode@gmail.com>
2023-06-22 07:01:03 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 /**
  * _isupper - is to let all char to be uppercase letter
  * @c: char to check
+ * Return: 1 or 0
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
 	return (0);
 }
-
