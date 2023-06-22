d396f21c65da0b44d07c726655af2ac45c8823dd
FreakyJay1 <jjthebestmanmode@gmail.com>
2023-06-22 02:08:45 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,12 @@
 #include "main.h"
 /**
- * _isupper: is to let all char to be uppercase letter
+ * _isupper- is to let all char to be uppercase letter
  * @c: char to check
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
