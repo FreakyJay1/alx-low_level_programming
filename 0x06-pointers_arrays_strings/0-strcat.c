b79e258572f177c12ae3775e72f703194b9b9473
FreakyJay1 <jjthebestmanmode@gmail.com>
2023-06-29 05:38:56 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,12 +4,12 @@
  * @dest: input value
  * @src: input value
  *
- * Return: void
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int i;
-	int k;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -17,11 +17,11 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 	j = 0;
-	while (src[k] != '\0')
+	while (src[j] != '\0')
 	{
-		dest[i] = src[k];
+		dest[i] = src[j];
 		i++;
-		k++;
+		j++;
 	}
 
 	dest[i] = '\0';
